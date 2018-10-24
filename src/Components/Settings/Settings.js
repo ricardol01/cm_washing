62ba6661fd7773e802e33eea80b88944ef0c1115
Khalil <khalilleo@users.noreply.github.com>
2018-10-24 15:18:44 -0400

    Added Settings View

--- src/Components/Settings/Settings.js
+++ src/Components/Settings/Settings.js
@@ -1,49 +1,97 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
-
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import {
+  Platform,
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+  FlatList
+} from 'react-native';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+import SettingsCell from './SettingsCell.js'
 
-type Props = {};
-export default class Home extends Component<Props> {
+export default class Home extends Component {
+  constructor(props) {
+    super(props);
+    const cellsData = [
+      {
+        icon: require("./Image/1.png"),
+        title: "我的订单",
+        key: "orders"
+      },
+      {
+        icon: require("./Image/1.png"),
+        title: "地址维护",
+        key: "address"
+      },
+      {
+        icon: require("./Image/1.png"),
+        title: "联系客服",
+        key: "contact"
+      },
+      {
+        icon: require("./Image/1.png"),
+        title: "甜满箱 全场免运费 满$25起送",
+        key: "sbox"
+      },
+      {
+        icon: require("./Image/1.png"),
+        title: "馋猫订餐",
+        key: "cmeat"
+      },
+      {
+        icon: require("./Image/1.png"),
+        title: "馋猫生活",
+        key: "cmlife"
+      },
+      {
+        icon: require("./Image/1.png"),
+        title: "选择语言&地区",
+        key: "language"
+      },
+      {
+        icon: require("./Image/1.png"),
+        title: "退出登录",
+        key: "logout"
+      },
+    ]
+    this.state = {
+      cells: cellsData
+    };
+  }
+  onPressedCell(key){
+    console.log(key);
+  }
+  renderCells(item) {
+    return (<SettingsCell cardStyle={styles.card} title={item.title} key={item.key} icon={item.icon} onPressedCell={this.onPressedCell} />)
+  }
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
+        <FlatList data={this.state.cells} renderItem={({item}) => (this.renderCells(item))}/>
       </View>
-    );
+
+    )
   }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    backgroundColor: '#F0F0F0',
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
+  card: {
+    flex: 1,
+    backgroundColor: 'white',
+    marginLeft: 12,
+    marginRight: 12,
     marginBottom: 5,
+    marginTop: 5,
+    borderRadius: 6,
+    elevation: 5,
+    shadowOffset: {width: 0, height: 0},
+    shadowColor: 'grey',
+    shadowOpacity: 0.3,
+    shadowRadius: 2
   },
 });
