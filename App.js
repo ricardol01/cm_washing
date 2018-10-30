d789c300df71231c19f726f1bdd80bba25326866
Khalil <khalilleo@users.noreply.github.com>
2018-10-30 15:22:36 -0400

    Removed duplicated files

--- App.js
+++ App.js
@@ -14,17 +14,16 @@ import Common from './src/Constants/Common'
 import Home from './src/Components/Home/Home';
 import Orders from './src/Components/Orders/Orders';
 import Settings from './src/Components/Settings/Settings';
+import Checkout from './src/Components/Checkout/Checkout';
+
 import CmWashingHomeAlert from './src/Components/HomeAlert/CmWashingHomeAlert';
 
-import CmWashingCartAlert from './src/Components/HomeAlert/CmWashingCartAlert';
-
-import CmWashingAddressAlert from './src/Components/HomeAlert/CmWashingAddressAlert';
 Navigation.registerComponent('home', () => Home);
 Navigation.registerComponent('orders', () => Orders);
 Navigation.registerComponent('settings', () => Settings);
+
 Navigation.registerComponent('CmWashingHomeAlert', () => CmWashingHomeAlert);
-Navigation.registerComponent('CmWashingAddressAlert', () => CmWashingAddressAlert);
-Navigation.registerComponent('CmWashingCartAlert', () => CmWashingCartAlert);
+
 Navigation.startTabBasedApp({
   tabs: [
     {
