86ac94dca05909c5cc080b121f5f9ed7b182f032
ricardol01 <mgu@yorku.ca>
2018-10-31 17:49:47 -0400

    merge

--- App.js
+++ App.js
@@ -15,7 +15,10 @@ import Home from './src/Components/Home/Home';
 import Orders from './src/Components/Orders/Orders';
 import Settings from './src/Components/Settings/Settings';
 import Checkout from './src/Components/Checkout/Checkout';
+
+
 import CmWashingHomeAlert from './src/Components/HomeAlert/CmWashingHomeAlert';
+
 Navigation.registerComponent('home', () => Home);
 Navigation.registerComponent('orders', () => Orders);
 Navigation.registerComponent('settings', () => Settings);
